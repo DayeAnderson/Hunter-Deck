86de0df86723838d693fe9402433aa01f6d37c76
David Anderson <djathree92@gmail.com>
2020-10-10 15:45:35 -0400

    add model and super user

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Hunter(models.Model):
+    name = models.CharField(max_length=100)
+    rank = models.IntegerField()
+    gender = models.CharField(max_length=100)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Hunter
 
 # Create your views here.
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def hunters_index(request):
+  hunters = Hunter.objects.all()
+  return render(request, 'hunters/index.html', { 'hunters': hunters })
+
+
